6fa7fcbebd173a2d80ba742f78a000bab2591787
Hinarin2017 <b812110011@tmu.edu.tw>
2025-04-26 22:18:21 +0800

    1.02

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919ee8b595e6182373fc909d787f88e8df2647ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed17d3d8b5880e4b4d4913af31225e644050e234")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

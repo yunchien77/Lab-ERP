6f9a9e69fa6559dee4420e8fb6767d1db7415f8b
yunchien77 <b812110004@tmu.edu.tw>
2025-04-28 22:40:08 +0800

    edit myequipments page

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e925786cb9cd1165a7514542212153415afb3020")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e43428274fd4f59bd7e83b3e8b3003db5a1f156")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
